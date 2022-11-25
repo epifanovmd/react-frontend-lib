b52f08aebe2166e50d4383ccf2a43ef1c27b8e89
Андрей Епифанов <epifanovmd@gmail.com>
2022-11-25 17:03:46 +0300

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,43 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "index.js",
-    libraryTarget: "umd",
-    library: "react-frontend-lib",
-    umdNamedDefine: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  externals: [nodeExternals()],
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: "babel-loader" },
-          {
-            loader: "ts-loader",
-            options: {},
-          },
-        ],
-      },
-    ],
-  },
-};
+function createConfig(filename, configFile) {
+  return {
+    mode: "development",
+    entry: "./src/index.ts",
+    output: {
+      path: path.resolve(__dirname, "lib"),
+      filename: "index.cjs.js",
+      libraryTarget: "commonjs",
+      library: "react-frontend-lib",
+      umdNamedDefine: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    externals: [nodeExternals()],
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: "babel-loader" },
+            {
+              loader: "ts-loader",
+              options: {
+                configFile: "tsconfig.cjs.json",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+}
+
+module.exports = [
+  createConfig("index.cjs.js", "tsconfig.cjs.json"),
+  createConfig("index.mjs.js", "tsconfig.mjs.json"),
+];
