8a61bc127b1024c4f6246a9938c713e2f42ff47b
Андрей Епифанов <epifanovmd@gmail.com>
2022-11-27 01:16:36 +0300

    refactoring

--- src/common/helpers/index.ts
+++ src/common/helpers/index.ts
@@ -2,7 +2,6 @@ export * from "./array";
 export * from "./debounce";
 export * from "./getInitialDataSSR";
 export * from "./numbers";
-export * from "./pluralize";
 export * from "./string";
 export * from "./typeGuards";
 export * from "./typeUtils";
--- src/common/helpers/string.ts
+++ src/common/helpers/string.ts
@@ -1,5 +1,39 @@
+export interface PluralOptions {
+  one: string;
+  two: string;
+  five: string;
+}
+
 export const stringToUpperCase = (str: string): string =>
   (str?.length || 0) > 0 ? str[0].toUpperCase() + str.slice(1) : str;
 
 export const stringToLowerCase = (str: string): string =>
   (str?.length || 0) > 0 ? str[0].toLowerCase() + str.slice(1) : str;
+
+export function pluralize(
+  count: number,
+  { one, two, five = two }: PluralOptions,
+  withNumber?: boolean,
+  hideZero?: boolean,
+) {
+  if (count === 0 && hideZero) {
+    return "";
+  }
+  let plural = Math.floor(Math.abs(count)) % 100;
+
+  if (plural > 10 && plural < 20) {
+    return withNumber ? `${count} ${five}` : five;
+  }
+
+  plural %= 10;
+
+  if (1 === plural) {
+    return withNumber ? `${count} ${one}` : one;
+  }
+
+  if (plural >= 2 && plural <= 4) {
+    return withNumber ? `${count} ${two}` : two;
+  }
+
+  return withNumber ? `${count} ${five}` : five;
+}
