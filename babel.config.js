0f6a439837dc0ef9d5e9d76ea4f02d6ec237d2f3
Андрей Епифанов <epifanovmd@gmail.com>
2022-12-05 11:09:51 +0300

    refactoring

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   presets: ["@babel/preset-typescript", "@babel/preset-react"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        root: ["./src"],
-      },
-    ],
-  ],
+  plugins: [],
 };
